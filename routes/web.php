9c1ca830a4facb19f3feb5a7b333bd95fc15c13f
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-08-06 20:56:30 +0900

    Update web.php
    第一引数　プロフィールファイル選択

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-//Route::get('/topprofile','HelloController@index');
+Route::get('/topprofile','HelloController@index');
