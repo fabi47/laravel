82f3209952b28fabf9ae68e23b88f32d9f294b7c
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-08-11 22:53:02 +0900

    Update HelloController.php
    controllerからviewに送る書き方（コメントアウト）

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,3 +10,23 @@ class HelloController extends Controller
         return view('topprofile');
     }
 }
+
+
+
+/*------------controllerからviewに送る----------------
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+  public function index() {
+$test_1 = "テスト1";
+$test_2 = "テスト2";
+       return view('test',compact('test_1', 'test_2'));
+   }
+
+}
+
+-----------------------------------------------------*/
